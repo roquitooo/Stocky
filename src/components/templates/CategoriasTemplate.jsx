4f8999f40c608c956d418f235e674a9f7e3bd107
Rocco Tommasi <roccotommasi@gmail.com>
2025-08-14 20:26:40 -0300

    21avo - 14/8 1- maquetado de la pagina de categorias

--- src/components/templates/CategoriasTemplate.jsx
+++ src/components/templates/CategoriasTemplate.jsx
@@ -2,10 +2,37 @@ import styled from "styled-components";
 
 export function CategoriasTemplate() {
     return (<Container>
-<span>Categorias Template</span>
+<section className="area1">
+area1
+</section>
+<section className="area2">
+area2
+</section>
+<section className="main">
+main
+</section>
     </Container>);
 }
 
 const Container = styled.div`
-
+    height: 100vh;
+    padding: 15px;
+    width: 100%;
+    display: grid;
+    grid-template:
+    "area1" 100px
+    "area2" 100px
+    "main" auto;
+    .area1 {
+        grid-area: area1;
+        background-color: rgb(206, 136, 33);
+    }
+    .area2 {
+        grid-area: area2;
+        background-color: rgb(236, 189, 119);
+    }
+    .main {
+        grid-area: main;
+        background-color: rgb(206, 136, 33);
+        }
 `
